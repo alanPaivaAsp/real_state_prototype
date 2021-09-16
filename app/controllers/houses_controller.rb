74833e241e6ac34e92c256e4970a4a63dcd99ff0
Alan <alan@contaswap.com>
2021-09-16 18:38:12 -0300

    feat: ♻️ remove create update and delete from houses_controller

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -13,31 +13,6 @@ class HousesController < ApplicationController
     render json: @house
   end
 
-  # POST /houses
-  def create
-    @house = House.new(house_params)
-
-    if @house.save
-      render json: @house, status: :created, location: @house
-    else
-      render json: @house.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /houses/1
-  def update
-    if @house.update(house_params)
-      render json: @house
-    else
-      render json: @house.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /houses/1
-  def destroy
-    @house.destroy
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_house
